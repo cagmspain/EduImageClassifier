b3bfbc245e9ecbc4bdeb6b8c8d772deb91126a4d
carlosGonzalez <104763487+cagmspain@users.noreply.github.com>
2024-09-20 23:41:00 +0200

    footer editado con enlace github y documentacion de proyecto

--- app.py
+++ app.py
@@ -95,6 +95,6 @@ if uploaded_file is not None:
 st.markdown("<hr>", unsafe_allow_html=True)
 st.markdown( """
     <p style='text-align: center;'>Esta aplicación usa el modelo MobileNetV2 preentrenado para clasificar imágenes en más de 1000 categorías.</p>
-    <p style='text-align: center;'>Código fuente disponible en <a href='https://github.com/tu_usuario/tu_repositorio' target='_blank'>GitHub</a>.</p>
+    <p style='text-align: center;'>Código fuente disponible en <a href='https://github.com/cagmspain/EduImageClassifier' target='_blank'>GitHub</a>.</p>
     """, 
     unsafe_allow_html=True)
